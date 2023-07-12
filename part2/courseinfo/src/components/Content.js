6bc4aa545b30e25aca86150cbead2ee7496dcba3
Victor Dominguez <vdomim@gmail.com>
2023-07-13 00:13:17 +0200

    Uploading exercises 2.2 and 2.3

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,11 +1,18 @@
 import Part from './Part'
 
 const Content = ({parts}) => {
+
 	return (
 		<div>
 			{parts.map(part => (
 				<Part key={part.id} name={part.name} exercises={part.exercises}/>
 			))}
+			<h2>total of {
+				parts
+				.map(part => part.exercises)
+				.reduce((total, exercises) => total+exercises, 0)
+			} exercises
+			</h2>
 		</div>
 	)	
 }
